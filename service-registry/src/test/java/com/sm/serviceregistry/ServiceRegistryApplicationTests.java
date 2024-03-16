576ec6f0ed93bf997cf57da61bafb478a325d400
Sayak Mondal <isayakmondal@gmail.com>
2024-03-16 11:26:05 +0530

    Add Service Registry (Eureka Server)

---
+++ service-registry/src/test/java/com/sm/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
